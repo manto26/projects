2d6679a68cbf1261f48cf7676460f443542515fc
Manto <manto26@gmail.com>
2022-09-20 11:33:44 +0300

    Tip calculator app

--- tip_calc/script.js
+++ tip_calc/script.js
@@ -1,7 +1,12 @@
-food = Number(prompt("How much was the food?"))
-tipPercentage = Number(prompt('Tip %?')) / 100
-tipAmount = food * tipPercentage
-total = food + tipAmount
-
-console.log('Tip Amount', tipAmount)
-console.log('Total', total)
+let calc = function () {
+    let bill = Number(document.getElementById('bill').value);
+    let split = Number(document.getElementById('split').value)
+    let tip = bill * .15;
+    let totalBill = bill + tip;
+    let splitPerPerson = totalBill / split
+    let btn = getElementById("b1");
+    btn.addEventListener("click", calc);
+    document.getElementById("tip").innerHTML = "$" + Number(tip).toFixed(2);
+    document.getElementById("total").innerHTML = "$" + Number(totalBill).toFixed(2);
+    document.getElementById("splitTotal").innerHTML = "$" + Number(splitPerPerson).toFixed(2);
+}  
